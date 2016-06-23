ca60b2e403c2a9ea2f47147eff7f56b2f183abb0
Iker Ibarguren <ikerib@gmail.com>
2016-06-23 08:58:50 +0200

    Loading spinner

--- src/AppBundle/Controller/KontzeptuaController.php
+++ src/AppBundle/Controller/KontzeptuaController.php
@@ -1,86 +1,90 @@
 <?php
 
-namespace AppBundle\Controller;
+    namespace AppBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use AppBundle\Entity\Kontzeptua;
-use AppBundle\Form\KontzeptuaType;
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+    use AppBundle\Entity\Kontzeptua;
+    use AppBundle\Form\KontzeptuaType;
 
-/**
- * Kontzeptua controller.
- *
- * @Route("/admin/kontzeptua")
- */
-class KontzeptuaController extends Controller
-{
     /**
-     * Creates a new Kontzeptua entity.
+     * Kontzeptua controller.
      *
-     * @Route("/new/{azpiatalaid}", options = { "expose" = true }, name="admin_kontzeptua_new")
-     * @Method({"GET", "POST"})
+     * @Route("/admin/kontzeptua")
      */
-    public function newAction(Request $request, $azpiatalaid)
+    class KontzeptuaController extends Controller
     {
-        $em = $this->getDoctrine();
 
-        $azpiatala = $em->getRepository( 'AppBundle:Kontzeptua' )->find( $azpiatalaid );
-        $kontzeptua = new Kontzeptua();
-        $kontzeptua->setAzpiatala( $azpiatala );
-        $kontzeptua->setUdala( $this->getUser()->getUdala() );
+        /**
+         * Creates a new Kontzeptua entity.
+         *
+         * @Route("/new/{azpiatalaid}", options = { "expose" = true }, name="admin_kontzeptua_new")
+         * @Method({"GET", "POST"})
+         */
+        public function newAction ( Request $request, $azpiatalaid )
+        {
+            $em = $this->getDoctrine();
 
-        $form = $this->createForm('AppBundle\Form\KontzeptuaType', $kontzeptua);
-        $form->handleRequest($request);
+            $azpiatala = $em->getRepository( 'AppBundle:Azpiatala' )->find( $azpiatalaid );
+            $kontzeptua = new Kontzeptua();
+            $kontzeptua->setAzpiatala( $azpiatala );
+            $kontzeptua->setUdala( $this->getUser()->getUdala() );
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($kontzeptua);
-            $em->flush();
+            $form = $this->createForm( 'AppBundle\Form\KontzeptuaType', $kontzeptua );
+            $form->handleRequest( $request );
+
+            if ( $form->isSubmitted() && $form->isValid() ) {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist( $kontzeptua );
+                $em->flush();
+
+                return $this->redirect( $request->headers->get( 'referer' ) );
+            }
+
+            return $this->render(
+                'kontzeptua/new.html.twig',
+                array (
+                    'kontzeptua'  => $kontzeptua,
+                    'azpiatalaid' => $azpiatalaid,
+                    'form'        => $form->createView(),
+                )
+            );
+        }
+
+        /**
+         * Deletes a Kontzeptua entity.
+         *
+         * @Route("/{id}", name="admin_kontzeptua_delete")
+         * @Method("DELETE")
+         */
+        public function deleteAction ( Request $request, Kontzeptua $kontzeptua )
+        {
+            $form = $this->createDeleteForm( $kontzeptua );
+            $form->handleRequest( $request );
+
+            if ( $form->isSubmitted() && $form->isValid() ) {
+                $em = $this->getDoctrine()->getManager();
+                $em->remove( $kontzeptua );
+                $em->flush();
+            }
 
             return $this->redirect( $request->headers->get( 'referer' ) );
         }
 
-        return $this->render('kontzeptua/new.html.twig', array(
-            'kontzeptua' => $kontzeptua,
-            'form' => $form->createView(),
-        ));
-    }
-
-    /**
-     * Deletes a Kontzeptua entity.
-     *
-     * @Route("/{id}", name="admin_kontzeptua_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, Kontzeptua $kontzeptua)
-    {
-        $form = $this->createDeleteForm($kontzeptua);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($kontzeptua);
-            $em->flush();
+        /**
+         * Creates a form to delete a Kontzeptua entity.
+         *
+         * @param Kontzeptua $kontzeptua The Kontzeptua entity
+         *
+         * @return \Symfony\Component\Form\Form The form
+         */
+        private function createDeleteForm ( Kontzeptua $kontzeptua )
+        {
+            return $this->createFormBuilder()
+                ->setAction( $this->generateUrl( 'admin_kontzeptua_delete', array ('id' => $kontzeptua->getId()) ) )
+                ->setMethod( 'DELETE' )
+                ->getForm();
         }
-
-        return $this->redirect( $request->headers->get( 'referer' ) );
     }
-
-    /**
-     * Creates a form to delete a Kontzeptua entity.
-     *
-     * @param Kontzeptua $kontzeptua The Kontzeptua entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(Kontzeptua $kontzeptua)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('admin_kontzeptua_delete', array('id' => $kontzeptua->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
-    }
-}
