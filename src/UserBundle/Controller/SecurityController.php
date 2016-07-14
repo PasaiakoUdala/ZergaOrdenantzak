30487191458f3c7b0412e227e9d0db029afbf2df
Gorka Gonzalez <ggonzalez@pasaia.net>
2016-07-14 10:04:36 +0200

    IZFE autentikazioa

--- src/UserBundle/Controller/SecurityController.php
+++ src/UserBundle/Controller/SecurityController.php
@@ -23,24 +23,47 @@
             $miurl = $query_str['host'].'/'.$query_str['path'];
             $query_str = parse_url( $request->getSession()->get( '_security.main.target_path' ), PHP_URL_QUERY );
 
-            if ( $query_str != null ) {
-
+            $urlOsoa= $request->getSession()->get( '_security.main.target_path' )."\n";            
+            
+            if (( $query_str != null )&&($this->container->getParameter('izfe_login_path')!='')) 
+            {
                 parse_str( $query_str, $query_params );
-
                 /* GET kodea*/
-                if ( $query_str != null ) {
-                    $valget = $query_params["kodea"];
-                    if ( $valget != "" ) {
-                        if ( $this->izfelogin( $valget, $miurl ) == 1 ) {
-                            return $this->redirectToRoute( 'admin_ordenantza_index' );
-                        }
+                if ( $query_str != null ) 
+                {
+//                    $valget = $query_params["kodea"];
+//                    if ( $valget != "" ) {
+//                        if ( $this->izfelogin( $valget, $miurl ) == 1 ) {
+//                            return $this->redirectToRoute( 'admin_ordenantza_index' );
+//                        }
+//                    }
+                    $NA=$query_params["DNI"];
+                    $udala=$query_params["AYUN"];
+                    $hizkuntza=$query_params["IDIOMA"];
+                    $fitxategia=$query_params["ficheroAuten"];
+
+                    if ($this->izfelogin ($NA,$udala,$hizkuntza,$fitxategia,$urlOsoa)==1)
+                    {
+                        return $this->redirectToRoute( 'admin_ordenantza_index' );
+                    }else
+                    {
+                        $lastUsername = null;
+                        $csrfToken = $this->get( 'security.csrf.token_manager' )->getToken( 'authenticate' )->getValue();
+                        $error = null;
+                        return $this->renderLogin(
+                            array (
+                                'last_username' => $lastUsername,
+                                'error'         => $error,
+                                'csrf_token'    => $csrfToken,
+                            )
+                        );
+//                        return $this->render( 'FOSUserBundle:Security:login.html.twig', $data );
                     }
                 }
             }
             else
             {
                 /** FOSUSERBUNDLE LoginAction */
-
                 /** @var $session \Symfony\Component\HttpFoundation\Session\Session */
                 $session = $request->getSession();
 
@@ -97,29 +120,59 @@
             return $this->render( 'FOSUserBundle:Security:login.html.twig', $data );
         }
 
-        private function izfelogin ( $valget )
+//        private function izfelogin ( $valget )
+//        {
+//            /* fitxategiko kodea */
+//            $client = new GuzzleHttp\Client();
+//            $res = $client->request( 'GET', 'http://obelix/izfe.txt' );
+//            if ( $res->getStatusCode() == 200 ) {
+//                $valftp = (string)$res->getBody();
+//                $valftp = str_replace( PHP_EOL, '', $valftp );
+//            }
+//
+//            /* Konparatu ta bestela errorea */
+//            if ( $valftp == $valget ) {
+//                $userManager = $this->container->get( 'fos_user.user_manager' );
+//                $user = $userManager->findUserByUsername( 'admin' );
+//                $token = new UsernamePasswordToken( $user, null, 'main', $user->getRoles() );
+//                $this->get( 'security.token_storage' )->setToken( $token );
+//                $this->get( 'session' )->set( '_security_main', serialize( $token ) );
+//
+//                return 1;
+//            }
+//
+//            return 0;
+//
+//        }
+        private function izfelogin($NA,$udala,$hizkuntza,$fitxategia,$urlOsoa)
         {
             /* fitxategiko kodea */
-            $client = new GuzzleHttp\Client();
-            $res = $client->request( 'GET', 'http://obelix/izfe.txt' );
-            if ( $res->getStatusCode() == 200 ) {
-                $valftp = (string)$res->getBody();
-                $valftp = str_replace( PHP_EOL, '', $valftp );
-            }
+//        $fitx = fopen($this->container->getParameter('izfe_login_path').'/'.$fitxategia,"r") or die("Unable to open file!");
+            /* fitxategia ez bada existitzen login orrira berbideratu */
 
-            /* Konparatu ta bestela errorea */
-            if ( $valftp == $valget ) {
-                $userManager = $this->container->get( 'fos_user.user_manager' );
-                $user = $userManager->findUserByUsername( 'admin' );
-                $token = new UsernamePasswordToken( $user, null, 'main', $user->getRoles() );
-                $this->get( 'security.token_storage' )->setToken( $token );
-                $this->get( 'session' )->set( '_security_main', serialize( $token ) );
+        dump($urlOsoa);
+            if (file_exists ($this->container->getParameter('izfe_login_path').'/'.$fitxategia))
+            {
+                $fitx = fopen($this->container->getParameter('izfe_login_path').'/'.$fitxategia,"r");
+                $lerro = fgets($fitx);
 
-                return 1;
-            }
+                /* fitxategiaren edukia eta url-a berdinak diren konparatu*/
+                if ($lerro == $urlOsoa)
+                {
+                    $userManager = $this->container->get('fos_user.user_manager');
+                    $user = $userManager->findUserByUsername($NA);
 
-            return 0;
+                    $token = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
+                    $this->get('security.token_storage')->setToken($token);
+                    $this->get('session')->set('_security_main', serialize($token));
 
-        }
+                    /* login-a egin ondoren fitxategia ezabatu */
+                    unlink($this->container->getParameter('izfe_login_path') . '/' . $fitxategia);
+                    return 1;
+                }
+                return 0;
+            } return 0;
+        }        
+        
 
     }
