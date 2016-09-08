4186dcaff168d8c1959ab6e488ca5dd19dab59f0
Iker Ibarguren <ikerib@gmail.com>
2016-09-08 13:44:36 +0200

    Controladorea sortu Odt-ak sortzeko funtzioarekin

--- src/FrontendBundle/Controller/DefaultController.php
+++ src/FrontendBundle/Controller/DefaultController.php
@@ -40,6 +40,57 @@ class DefaultController extends Controller
     }
 
     /**
+     * Finds and displays a Ordenantza entity (OFT).
+     *
+     * @Route("/{udala}/{_locale}/odt", name="frontend_ordenantza_odt",
+     *         requirements={
+     *           "_locale": "eu|es",
+     *           "udala": "\d+"
+     *          }
+     * )
+     * @Method("GET")
+     */
+    public function odtAction($udala)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery('
+          SELECT o FROM AppBundle:Ordenantza o LEFT JOIN AppBundle:Udala u  WITH o.udala=u.id
+            WHERE u.kodea = :udala
+            ORDER BY o.kodea ASC
+        ');
+        $query->setParameter('udala', $udala);
+        $ordenantzas = $query->getResult();
+
+        $pdf = $this->get("white_october.tcpdf")->create('vertical', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+        $pdf->SetAuthor($udala);
+        $pdf->SetTitle(date("Y")."-Zerga Ordenantzak");
+
+        $pdf->setFontSubsetting(true);
+        $pdf->SetFont('helvetica', '', 11, '', true);
+
+        $pdf->setHeaderData('',0,'','',array(0,0,0), array(255,255,255) );
+
+        $pdf->AddPage();
+
+        $azala = $this->render('frontend/azala.html.twig',array('eguna'=>date("Y"),'udala'=>$this->getUser()->getUdala()));
+        $pdf->writeHTMLCell($w = 0, $h = 0, $x = '', $y = '', $azala->getContent(), $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
+        $pdf->AddPage();
+
+        foreach ($ordenantzas as $ordenantza)
+        {
+            $mihtml = $this->render('frontend/pdf.html.twig', array('ordenantza' => $ordenantza));
+            $pdf->writeHTMLCell($w = 0, $h = 0, $x = '', $y = '', $mihtml->getContent(), $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
+            $pdf->AddPage();
+        }
+        $filename = $udala."-".date("Y")."ko Zerga Ordenantzak";
+
+        $pdf->Output($filename.".pdf",'I'); // This will output the PDF as a response directly
+    }
+
+
+
+    /**
      * Finds and displays a Ordenantza entity (PDF).
      *
      * @Route("/{udala}/{_locale}/pdf", name="frontend_ordenantza_pdf",
