994321cc4fc4efa7506de4670e09c5bdfe25cedc
Iker Ibarguren <ikerib@gmail.com>
2016-09-12 13:57:58 +0200

    ROLE_SUPER_ADMIN editatzeko aukera

---
+++ src/UserBundle/Form/SuperuserType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
+
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+
+
+class SuperuserType extends AbstractType
+//class UserType extends BaseType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+        $builder
+            ->add('username')
+            ->add('udala')
+            ->add('enabled')
+            ->add('email')
+            ->add('roles',  ChoiceType::class, array(
+                'multiple' => true,
+                'choices'  => array(
+                    'Super Admin' => 'ROLE_SUPER_ADMIN',
+                    'Admin' => 'ROLE_ADMIN',
+                    'Erabiltzailea' => 'ROLE_USER'
+                ),
+            ))
+            ->add('password', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options'  => array('label' => 'Pasahitza'),
+                'second_options' => array('label' => 'Pasahitza Errepikatu'),
+            ))
+
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User'
+        ));
+    }
+}
